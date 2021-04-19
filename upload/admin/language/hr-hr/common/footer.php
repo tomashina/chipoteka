d402d705236529eb3d94d5e8ea90c1d696a8701d
tomashina <tomislav@agmedia.hr>
2021-04-19 15:35:08 +0200

    fixes admin

---
+++ upload/admin/language/hr-hr/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="https://www.chipoteka.hr/">Chipoteka d.o.o.</a> &copy; ' . date('Y') . '. Sva prava pridržana.';
+$_['text_version'] = 'Verzija %s';
---
+++ upload/catalog/language/hr-hr/checkout/success.php
@@ -0,0 +1,11 @@
+<?php
+// Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
+// Heading
+$_['heading_title']        = 'Vaša narudžba je zaprimljena!';
+
+// Text
+$_['text_basket']          = 'Košarica';
+$_['text_checkout']        = 'Naplata';
+$_['text_success']         = 'Uspješno';
+$_['text_customer']        = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Možete pogledati pregled Vaših narudžbi na stranici <a href="%s">moj korisnički račun</a> ili kliknuvši na <a href="%s">povijest narudžbi</a>.</p><p style="display:none">Ako Vaša narudžba sadrži artikle za download, možete otići na <a href="%s">download</a> stranicu da ih pogledate i preuzmete.</p><p>Molimo Vas da ukoliko imate nekih pitanja ista uputite <a href="%s">vlasniku web trgovine</a>.</p><p>Zahvaljujemo se što ste kupovali online kod nas!</p>';
+$_['text_guest']           = '<p>Vaša narudžba je uspješno zaprimljena i obrađena!</p><p>Molimo Vas da ukoliko imate nekih pitanja ista uputite <a href="%s">vlasniku web trgovine</a>.</p><p>Zahvaljujemo se što ste kupovali online kod nas!</p>';
