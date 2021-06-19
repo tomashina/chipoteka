6ea8bc5e3b7765ad22dc89c56db2ee915c67916f
tomashina <tomislav@agmedia.hr>
2021-06-19 07:50:54 +0200

    cache

--- storage/cache/template/75/75027a270c3d7c9426aba385b094f6cc31b7f94fa4e044bd1ad67f6d9e9980f5.php
+++ storage/cache/template/75/75027a270c3d7c9426aba385b094f6cc31b7f94fa4e044bd1ad67f6d9e9980f5.php
@@ -38,136 +38,111 @@ class __TwigTemplate_4339078212f97602ee1a51ec02cb6abe669d19bc8cceee7f68c74363d6a
 <div class=\"col-12  product-card-wrapper\">
 
     <div class=\"card product-card\">
-      <a class=\"stretched-link\" href=\"";
-        // line 5
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "href", [], "any", false, false, false, 5);
-        echo "\">
-
-        ";
-        // line 8
-        echo "
-            <div class=\"position-relative\">
-
-                <button class=\"btn btn-primary btn-basket\" type=\"button\" onclick=\"cart.add('";
-        // line 11
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "product_id", [], "any", false, false, false, 11);
+        <div class=\"position-relative\" >
+            <button class=\"btn btn-primary btn-basket \"  type=\"button\" onclick=\"cart.add('";
+        // line 6
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "product_id", [], "any", false, false, false, 6);
         echo "', '";
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "minimum", [], "any", false, false, false, 11);
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "minimum", [], "any", false, false, false, 6);
         echo "');\"><i class=\"fad fs-xl fa-shopping-basket\"></i><span class=\"sr-only\" ";
         echo ($context["button_cart"] ?? null);
         echo "</span></button>
-
-                <div class=\"card-img-top d-block overflow-hidden\" href=\"";
+            <div class=\"card-img-top d-block overflow-hidden\" ><img src=\"";
+        // line 7
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "thumb", [], "any", false, false, false, 7);
+        echo "\" alt=\"";
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "name", [], "any", false, false, false, 7);
+        echo "\"></div>
+            <div class=\"product-badge-cpta product-badge-cpta__free-shipping\">
+                <span> Besplatna dostava</span>
+                <i class=\"fas fa-shipping-fast\"></i>
+            </div>
+        </div>
+      <a class=\"stretched-link\"  href=\"";
         // line 13
         echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "href", [], "any", false, false, false, 13);
-        echo "\"><img src=\"";
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "thumb", [], "any", false, false, false, 13);
-        echo "\" alt=\"";
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "name", [], "any", false, false, false, 13);
-        echo "\"></div>
-
-                <div class=\"product-badge-cpta product-badge-cpta__free-shipping\">
-                     <span> Besplatna dostava</span>
-                     <i class=\"fas fa-shipping-fast\"></i>    
-                </div>
-
-            </div>
-
+        echo "\">
+        ";
+        // line 15
+        echo "
         <div class=\"card-body\">
-            <h3 class=\"product-title\"><a href=\"";
-        // line 23
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "href", [], "any", false, false, false, 23);
-        echo "\">";
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "name", [], "any", false, false, false, 23);
-        echo "</a></h3>
-
+            <h3 class=\"product-title\">";
+        // line 17
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "name", [], "any", false, false, false, 17);
+        echo "</h3>
                 ";
-        // line 25
-        if (twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 25)) {
-            // line 26
-            echo "
-                    <div class=\"product-price\">
+        // line 18
+        if (twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 18)) {
+            // line 19
+            echo "                    <div class=\"product-price\">
                         ";
-            // line 28
-            if (twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 28)) {
-                // line 29
+            // line 20
+            if (twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 20)) {
+                // line 21
                 echo "                            <span>";
-                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 29);
+                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 21);
                 echo "</span>
                             <span><del>";
-                // line 30
-                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 30);
+                // line 22
+                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 22);
                 echo "</del></span>
                         ";
             } else {
-                // line 32
+                // line 24
                 echo "                            <span> ";
-                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 32);
+                echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 24);
                 echo "</span>
                         ";
             }
-            // line 34
+            // line 26
             echo "                    </div><!-- .price -->
-
                 ";
         }
-        // line 37
-        echo "                ";
-        if (twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "rating", [], "any", false, false, false, 37)) {
-            // line 38
-            echo "
-                <div class=\"star-rating ";
-            // line 39
-            echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "rating", [], "any", false, false, false, 39);
-            echo "\"><i class=\"star-rating-icon ci-star-filled active\"></i><i class=\"star-rating-icon ci-star-filled active\"></i><i class=\"star-rating-icon ci-star-filled active\"></i><i class=\"star-rating-icon ci-star-filled active\"></i><i class=\"star-rating-icon ci-star\"></i>
+        // line 28
+        echo "
+            ";
+        // line 29
+        if (((twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 29) && twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 29)) && ($context["salebadge_status"] ?? null))) {
+            // line 30
+            echo "                <div class=\"product-badge-cpta product-badge-cpta__discount\">
+                    <span> Black Friday</span>
+                    <span> Uštedi <strong>";
+            // line 32
+            echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "sale_badge", [], "any", false, false, false, 32);
+            echo "</strong></span>
                 </div>
-                ";
+            ";
         }
-        // line 42
+        // line 35
         echo "
 
-                <div class=\"product-badge-cpta product-badge-cpta__discount\">
-                    <span> Black Friday</span>
-                    <span> Uštedi <strong>54%</strong></span>
-                </div>
-
-
             <div class=\"product-price--web\">
-
                 <span>
                     Web cijena: 
                 </span>
                 <span>
                     ";
-        // line 56
-        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "price", [], "any", false, false, false, 56);
+        // line 42
+        echo twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "special", [], "any", false, false, false, 42);
         echo "
                 </span>
             </div>
-
- 
+        </div>
+        
+        <div class=\"product-delivery-time\">
+            <i class=\"fas fa-shipping-timed\"></i>
+               <div class=\"d-flex flex-column\">
+               <span>
+               Proizvod kod dobavljača
+               </span>
+               <span>
+              Šaljemo do 05.05.2021
+               </span>
+               </div>
         </div>
 
-
-        <div class=\"product-delivery-time\">
-
-    <i class=\"fas fa-shipping-timed\"></i>
-       <div class=\"d-flex flex-column\">
-       <span>
-       Proizvod kod dobavljača 
-       </span>
-
-       <span>
-      Šaljemo do 05.05.2021
-       </span>
-       </div>
-
-   </div>
-
-
     </a>
-          
-       
+
   </div>
     
 </div>";
@@ -185,7 +160,7 @@ class __TwigTemplate_4339078212f97602ee1a51ec02cb6abe669d19bc8cceee7f68c74363d6a
 
     public function getDebugInfo()
     {
-        return array (  143 => 56,  127 => 42,  121 => 39,  118 => 38,  115 => 37,  110 => 34,  104 => 32,  99 => 30,  94 => 29,  92 => 28,  88 => 26,  86 => 25,  79 => 23,  62 => 13,  53 => 11,  48 => 8,  43 => 5,  37 => 1,);
+        return array (  126 => 42,  117 => 35,  111 => 32,  107 => 30,  105 => 29,  102 => 28,  98 => 26,  92 => 24,  87 => 22,  82 => 21,  80 => 20,  77 => 19,  75 => 18,  71 => 17,  67 => 15,  63 => 13,  52 => 7,  44 => 6,  37 => 1,);
     }
 
     public function getSourceContext()
