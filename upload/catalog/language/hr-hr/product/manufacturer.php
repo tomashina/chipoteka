2bc9127d3cf9c906c798e7bb33205033ea55b02d
tomashina <tomislav@agmedia.hr>
2021-07-21 12:34:14 +0200

    fix brands lang

--- upload/catalog/language/hr-hr/product/manufacturer.php
+++ upload/catalog/language/hr-hr/product/manufacturer.php
@@ -1,16 +1,16 @@
 <?php
 // Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
 // Heading
-$_['heading_title']     = 'Pronađite svoje omiljene proizvođače';
+$_['heading_title']     = 'Pronađite svoje omiljene brandove';
 
 // Text
-$_['text_brand']        = 'Proizvođač';
-$_['text_index']        = 'Popis proizvođača:';
-$_['text_error']        = 'Proizvođač nije pronađen!';
-$_['text_empty']        = 'Nema proizvođača za prikaz.';
+$_['text_brand']        = 'Brandovi';
+$_['text_index']        = 'Popis brandova:';
+$_['text_error']        = 'Brand nije pronađen!';
+$_['text_empty']        = 'Nema artikala za prikaz.';
 $_['text_quantity']     = 'Kol:';
-$_['text_manufacturer'] = 'Proizvođač:';
-$_['text_model']        = 'Model:';
+$_['text_manufacturer'] = 'Brand:';
+$_['text_model']        = 'Šifra:';
 $_['text_points']       = 'Nagradni bodovi:';
 $_['text_price']        = 'Cijena:';
 $_['text_tax']          = 'Bez poreza:';
