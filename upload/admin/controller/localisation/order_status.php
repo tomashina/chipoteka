b2c1f6572ce7e441050bf8cb89756bade57eebea
tomashina <tomislav@agmedia.hr>
2021-08-20 15:53:20 +0200

    validacija error

--- upload/admin/controller/localisation/order_status.php
+++ upload/admin/controller/localisation/order_status.php
@@ -329,7 +329,7 @@ class ControllerLocalisationOrderStatus extends Controller {
 		}
 
 		foreach ($this->request->post['order_status'] as $language_id => $value) {
-			if ((utf8_strlen($value['name']) < 3) || (utf8_strlen($value['name']) > 32)) {
+			if ((utf8_strlen($value['name']) < 3) || (utf8_strlen($value['name']) > 64)) {
 				$this->error['name'][$language_id] = $this->language->get('error_name');
 			}
 		}
