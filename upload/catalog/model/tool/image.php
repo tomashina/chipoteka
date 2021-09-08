287cb378e276e5e78bd573707155c87dfba24dcf
tomashina <tomislav@agmedia.hr>
2021-09-08 09:15:27 +0200

    imagae back

--- upload/catalog/model/tool/image.php
+++ upload/catalog/model/tool/image.php
@@ -41,8 +41,8 @@ class ModelToolImage extends Model {
 		$image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
 		
 		if ($this->request->server['HTTPS']) {
-			//return $this->config->get('config_ssl') . 'image/' . $image_new;
-            return 'https://cdn.chipoteka.hr/' . $image_new . '" width="' . $width . '" height="' . $height;
+			return $this->config->get('config_ssl') . 'image/' . $image_new;
+            //return 'https://cdn.chipoteka.hr/' . $image_new . '" width="' . $width . '" height="' . $height;
 		} else
 			return $this->config->get('config_url') . 'image/' . $image_new;
 		}
