03690db2b9a21ff3c6edc41cad6118c9e64956c8
tomashina <tomislav@agmedia.hr>
2021-09-13 08:13:24 +0200

    adress validation and footer fixes

--- upload/catalog/controller/checkout/guest.php
+++ upload/catalog/controller/checkout/guest.php
@@ -189,9 +189,12 @@ class ControllerCheckoutGuest extends Controller {
             }
 
 
-            if ((utf8_strlen(trim($this->request->post['address_1'])) < 3) || (utf8_strlen(trim($this->request->post['address_1'])) > 128)) {
+            if ((utf8_strlen(trim($this->request->post['address_1'])) < 3) || (utf8_strlen(trim($this->request->post['address_1'])) > 128) && (preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $this->request->post['address_1']))) {
 				$json['error']['address_1'] = $this->language->get('error_address_1');
 			}
+
+
+
 
 			if ((utf8_strlen(trim($this->request->post['city'])) < 2) || (utf8_strlen(trim($this->request->post['city'])) > 128)) {
 				$json['error']['city'] = $this->language->get('error_city');
