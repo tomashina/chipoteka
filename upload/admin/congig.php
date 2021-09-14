e1f8296e07a615519a6629ec1eb6f902691279c7
tomashina <tomislav@agmedia.hr>
2021-09-14 09:29:10 +0200

    config

---
+++ upload/admin/congig.php
@@ -0,0 +1,41 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http_server');
+define('HTTP_CATALOG', 'http_catalog');
+
+// HTTPS
+define('HTTPS_SERVER', 'https_server');
+define('HTTPS_CATALOG', 'https_catalog');
+
+// DIR
+define('DIR_APPLICATION', 'dir_application');
+define('DIR_SYSTEM', 'dir_system');
+define('DIR_IMAGE', 'dir_image');
+define('DIR_STORAGE', 'dir_storage');
+define('DIR_CATALOG', 'dir_catalog');
+define('DIR_LANGUAGE', 'dir_language');
+define('DIR_TEMPLATE', 'dir_template');
+define('DIR_CONFIG', 'dir_config');
+define('DIR_CACHE', 'dir_cache');
+define('DIR_DOWNLOAD', 'dir_download');
+define('DIR_LOGS', 'dir_logs');
+define('DIR_MODIFICATION', 'dir_modification');
+define('DIR_SESSION', 'dir_session');
+define('DIR_UPLOAD', 'dir_upload');
+
+// DB
+define('DB_DRIVER', 'db_driver');
+define('DB_HOSTNAME', 'db_host');
+define('DB_USERNAME', 'db_username');
+define('DB_PASSWORD', 'db_password');
+define('DB_DATABASE', 'db_database');
+define('DB_PORT', 'db_port');
+define('DB_PREFIX', 'db_prefix');
+
+define('CACHE_HOSTNAME', 'cache_host_name');
+define('CACHE_PORT', 'cache_host_port');
+define('CACHE_PREFIX', 'cache_host_prefix');
+define('CACHE_PASSWORD', 'cache_host_password');
+
+// OpenCart API
+define('OPENCART_SERVER', 'opencart_server');
