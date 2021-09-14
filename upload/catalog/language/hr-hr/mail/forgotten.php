a50614d46817a59b9aa586351eeea8690d432c8f
tomashina <tomislav@agmedia.hr>
2021-09-14 09:53:36 +0200

    forgoten mail

--- upload/catalog/language/hr-hr/mail/forgotten.php
+++ upload/catalog/language/hr-hr/mail/forgotten.php
@@ -5,4 +5,4 @@ $_['text_subject']  = '%s - Zatraženo resetiranje lozinke';
 $_['text_greeting'] = 'Nova lozinka zatražena je za %s korisnički račun.';
 // $_['text_password'] = 'Vaša nova lozinka je:'; // postojalo u verziji OC 2.1.0.1
 $_['text_change']   = 'Da biste resetirali Vašu lozinku kliknite na poveznicu (link) ispod:';
-$_['text_ip']       = 'Ovaj zahtjev došao je s IP adrese: %s';
+$_['text_ip']       = 'Ovaj zahtjev došao je s IP adrese:';
