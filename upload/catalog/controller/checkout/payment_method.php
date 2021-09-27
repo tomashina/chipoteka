9e0df41a6502a8b117fbac9b1aa228389019a62f
tomashina <tomislav@agmedia.hr>
2021-09-27 12:08:52 +0200

    test comit

--- upload/catalog/controller/checkout/payment_method.php
+++ upload/catalog/controller/checkout/payment_method.php
@@ -139,14 +139,12 @@ class ControllerCheckoutPaymentMethod extends Controller {
 
         if ($this->cart->getSubTotal() && (isset($this->session->data['creditcardname'] ) && $this->session->data['creditcardname']!='' )  && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0000' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0200' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0300' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0400' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0500' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0600' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0700' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0800' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='0900' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='1000' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='1100' ) && (isset($this->session->data['paymentplan'] ) && $this->session->data['paymentplan']!='1200' )) {
 
-
             if($this->session->data['payment_method']['code']=='wspay'){
                 $pricecalculate = $this->config->get('total_low_order_fee_fee') /100  * $this->cart->getSubTotal();
             }
             else{
                 $pricecalculate = $this->cart->getSubTotal();
             }
-
 
             $data['totalno'] = $this->cart->getSubTotal();
 
