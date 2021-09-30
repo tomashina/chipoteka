254d96d87f5efc2acdb89dcb5d9227289bab05b0
Filip Jankoski <filip@agmedia.hr>
2021-09-30 08:44:40 +0200

    Fix update.

--- storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_ProductSingle.php
+++ storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_ProductSingle.php
@@ -84,7 +84,7 @@ class LOC_ProductSingle
         $this->product_to_update = Product::whereIn('luceed_uid', $uids_list)->where('updated', 0)->first();
 
         if ($this->product_to_update && isset($this->product_to_update['luceed_uid'])) {
-            $this->luceed_product = LuceedProduct::where('uid', $this->product_to_update['luceed_uid'])
+            $this->luceed_product = LuceedProduct::query()->where('uid', '==', $this->product_to_update['luceed_uid'])
                                                  ->where('hash', '!=', $this->product_to_update['hash'])
                                                  ->first();
 
