173de59ba90339aec8ff9463247eeb65dc8c245a
Filip Jankoski <filip@agmedia.hr>
2021-10-07 14:52:31 +0200

    Fix update image.

--- storage/vendor/agmedia/luceed-opencart-wrapper/src/Helpers/ProductHelper.php
+++ storage/vendor/agmedia/luceed-opencart-wrapper/src/Helpers/ProductHelper.php
@@ -256,16 +256,31 @@ class ProductHelper
         $response = [];
         $docs  = collect($product['dokumenti']);
 
-        Log::store($docs, 'product_image');
-
         if ($docs->count()) {
-            for ($i = 0; $i < $docs->count(); $i++) {
+            /*for ($i = 0; $i < $docs->count(); $i++) {
                 Log::store(substr($product['dokumenti'][$i]->filename, -3), 'product_image');
                 if (isset($product['dokumenti'][$i]->filename) && substr($product['dokumenti'][$i]->filename, -3) == 'pdf') {
                     if (isset($product['dokumenti'][$i]->file_uid)) {
                         $uid = $product['dokumenti'][$i]->file_uid;
                     } else {
                         $uid = $product['dokumenti'][$i]['file_uid'];
+                    }
+
+                    $response[] = [
+                        'uid'        => $uid,
+                        'image'      => static::getImagePath($product, $i),
+                        'sort_order' => $i
+                    ];
+                }
+            }*/
+
+            foreach ($docs as $doc) {
+                Log::store(substr($doc['filename'], -3), 'product_image');
+                if (isset($doc['file_uid']) && substr($doc['filename'], -3) == 'pdf') {
+                    if (isset($doc['file_uid'])) {
+                        $uid = $doc['file_uid'];
+                    } else {
+                        $uid = $doc['file_uid'];
                     }
 
                     $response[] = [
