fa460d6c12d7c86f07cfffc0129e4ad7a4bd2079
Filip Jankoski <filip@agmedia.hr>
2021-10-07 14:57:35 +0200

    fix.

--- storage/vendor/agmedia/luceed-opencart-wrapper/src/Helpers/ProductHelper.php
+++ storage/vendor/agmedia/luceed-opencart-wrapper/src/Helpers/ProductHelper.php
@@ -276,7 +276,7 @@ class ProductHelper
 
             foreach ($docs as $doc) {
                 Log::store(substr($doc['filename'], -3), 'product_image');
-                if (isset($doc['file_uid']) && substr($doc['filename'], -3) == 'pdf') {
+                if (isset($doc['file_uid']) && substr($doc['filename'], -3) != 'pdf') {
                     if (isset($doc['file_uid'])) {
                         $uid = $doc['file_uid'];
                     } else {
