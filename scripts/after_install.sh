46d1ac9553fb477299d39d7f92761ba2caf77ded
tomashina <tomislav@agmedia.hr>
2021-10-16 17:14:32 +0200

    after install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,8 @@
 set -e
 echo "starting after_install.sh script"
 wget -O /dev/null https://testing.chipoteka.hr/admin/index.php?route=marketplace/modification/refreshcron
+wget -O /dev/null https://testing.chipoteka.hr/admin/index.php?route=marketplace/modification/refreshcron
+wget -O /dev/null https://testing.chipoteka.hr/admin/index.php?route=marketplace/modification/refreshcron
+wget -O /dev/null https://testing.chipoteka.hr/admin/index.php?route=marketplace/modification/refreshcron
+wget -O /dev/null https://testing.chipoteka.hr/admin/index.php?route=marketplace/modification/refreshcron
 echo "done with after_install.sh script"
