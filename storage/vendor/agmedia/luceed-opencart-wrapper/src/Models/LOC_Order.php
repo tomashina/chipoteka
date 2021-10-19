46d6d692b32214dd3a7ff34baf4e338321434557
tomashina <tomislav@agmedia.hr>
2021-10-19 15:47:18 +0200

    order id change

--- storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_Order.php
+++ storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_Order.php
@@ -170,7 +170,7 @@ class LOC_Order
         $this->items_available = false;//$this->setAvailability();
 
         $this->order = [
-            'nalog_prodaje_b2b'         => $this->oc_order['order_id'],
+            'nalog_prodaje_b2b'         => $this->oc_order['order_id']. '-' . Carbon::now()->year,
             'na_uvid'                   => $this->oc_order['order_id'] . '-' . Carbon::now()->year,
             'datum'                     => Carbon::make($this->oc_order['date_added'])->format(agconf('luceed.date')),
             'skladiste'                 => '001',
