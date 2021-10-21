7ec8fb58219472b2c3657a33e6e24ffcd2cdecf9
tomashina <tomislav@agmedia.hr>
2021-10-21 10:37:40 +0200

    test commit

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,4 @@
 set -e
 echo "starting after_install.sh script"
 wget -O /dev/null http://localhost/admin/index.php?route=marketplace/modification/refreshcron --header 'X-Forwarded-Proto: https' --header 'Host: www.chipoteka.hr'
-wget -O /dev/null http://localhost/admin/index.php?route=marketplace/modification/refreshcron --header 'X-Forwarded-Proto: https' --header 'Host: www.chipoteka.hr'
-wget -O /dev/null http://localhost/admin/index.php?route=marketplace/modification/refreshcron --header 'X-Forwarded-Proto: https' --header 'Host: www.chipoteka.hr'
 echo "done with after_install.sh script"
