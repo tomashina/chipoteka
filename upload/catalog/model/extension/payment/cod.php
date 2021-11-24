649ea25491f29a30aa872b3f134a1033d97a30ac
tomashina <tomislav@agmedia.hr>
2021-11-24 11:00:17 +0100

    payments vp fix

--- upload/catalog/model/extension/payment/cod.php
+++ upload/catalog/model/extension/payment/cod.php
@@ -3,13 +3,24 @@ class ModelExtensionPaymentCOD extends Model {
 	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/cod');
 
+        if ($this->customer->isLogged()) {
+            $data['groupId'] = $this->customer->getGroupId();
+
+        } else {
+            $data['groupId'] ='0';
+        }
+
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
 		if ($this->config->get('payment_cod_total') > 0 && $this->config->get('payment_cod_total') < $total) {
 			$status = false;
 		} elseif (!$this->cart->hasShipping()) {
 			$status = false;
-		} elseif (!$this->config->get('payment_cod_geo_zone_id')) {
+		}
+        elseif ($data['groupId'] >= 2) {
+            $status = false;
+        }
+		elseif (!$this->config->get('payment_cod_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
--- upload/catalog/model/extension/payment/wspay.php
+++ upload/catalog/model/extension/payment/wspay.php
@@ -3,10 +3,19 @@ class ModelExtensionPaymentWSPay extends Model {
 	public function getMethod($address, $total) {
 		$this->language->load('extension/payment/wspay');
 
+        if ($this->customer->isLogged()) {
+            $data['groupId'] = $this->customer->getGroupId();
+
+        } else {
+            $data['groupId'] ='0';
+        }
+
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('wspay_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
 		if ($this->config->get('wspay_total') > 0 && $this->config->get('wspay_total') > $total) {
 			$status = false;
+        } elseif ($data['groupId'] >= 2) {
+            $status = false;
 		} elseif (!$this->config->get('wspay_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
