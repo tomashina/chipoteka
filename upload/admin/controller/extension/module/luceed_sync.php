2198266d37dbbe3379fd5379b5b781aa9fde5315
tomashina <tomislav@agmedia.hr>
2022-01-10 13:45:18 +0100

    mail fixes

--- upload/admin/controller/extension/module/luceed_sync.php
+++ upload/admin/controller/extension/module/luceed_sync.php
@@ -721,6 +721,8 @@ class ControllerExtensionModuleLuceedSync extends Controller
                 $data['b2b_products'] = $lc->setDocument($data['luceed_uid'], $is_b2b)->sortProducts($data['products']);
             }
 
+            \Agmedia\Helpers\Log::store($data);
+
             $mail                = new Mail($this->config->get('config_mail_engine'));
             $mail->parameter     = $this->config->get('config_mail_parameter');
             $mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
