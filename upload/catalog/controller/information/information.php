31215de51640f4f3c961d49185fa5391e0813a4c
tomashina <tomislav@agmedia.hr>
2022-03-10 10:35:57 +0100

    seo fixes

--- upload/catalog/controller/information/information.php
+++ upload/catalog/controller/information/information.php
@@ -25,6 +25,8 @@ class ControllerInformationInformation extends Controller {
 			$this->document->setDescription($information_info['meta_description']);
 			$this->document->setKeywords($information_info['meta_keyword']);
 
+            $this->document->addLink($this->url->link('information/information', 'information_id=' .  $information_id), 'canonical');
+
 			$data['breadcrumbs'][] = array(
 				'text' => $information_info['title'],
 				'href' => $this->url->link('information/information', 'information_id=' .  $information_id)
