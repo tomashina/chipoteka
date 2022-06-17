e86a889b190a1ff3e8973df902b8666965914f0e
tomashina <tomislav@agmedia.hr>
2022-06-17 12:11:45 +0200

    facebook feed

--- upload/catalog/controller/extension/feed/facebookstore.php
+++ upload/catalog/controller/extension/feed/facebookstore.php
@@ -129,7 +129,7 @@ class ControllerExtensionFeedFacebookstore extends Controller {
         $length = strlen($value);
         for ($i=0; $i < $length; $i++)
         {
-            $current = ord($value{$i});
+            $current = ord($value[$i]);
             if (($current == 0x9) ||
                 ($current == 0xA) ||
                 ($current == 0xD) ||
