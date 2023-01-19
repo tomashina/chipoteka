e4cb9cfe6bab479f6ce811af3d5e43887dcd7a34
Filip Jankoski <filip@agmedia.hr>
2023-01-19 08:14:35 +0100

    __fix: menu OSOBNA NJEGA

--- upload/catalog/controller/extension/basel/default_menu.php
+++ upload/catalog/controller/extension/basel/default_menu.php
@@ -1,101 +1,100 @@
 <?php
-class ControllerExtensionBaselDefaultMenu extends Controller {
-	public function index() {
-		$this->load->language('common/menu');
 
-		// Menu
-		$this->load->model('catalog/category');
+class ControllerExtensionBaselDefaultMenu extends Controller
+{
 
-		$this->load->model('catalog/product');
+    public function index()
+    {
+        $this->load->language('common/menu');
+
+        // Menu
+        $this->load->model('catalog/category');
+        $this->load->model('catalog/product');
 
         if ($this->customer->isLogged()) {
             $data['groupId'] = $this->customer->getGroupId();
-
         } else {
-            $data['groupId'] ='0';
+            $data['groupId'] = '0';
         }
 
-		$data['categories'] = array();
+        $data['categories'] = array();
 
-		$categories = $this->model_catalog_category->getCategories(0);
+        $categories = $this->model_catalog_category->getCategories(0);
 
         if (isset ($this->request->get['path'])) {
-            $path = $this->request->get['path'];
-            $cats = explode('_', $path);
-            $data['cats'] = $cats;
+            $path           = $this->request->get['path'];
+            $cats           = explode('_', $path);
+            $data['cats']   = $cats;
             $data['cat_id'] = $cats[count($cats) - 1];
-
         }
 
-		foreach ($categories as $category) {
-			if ($category['top']) {
-				
-				// Level 2
-				$children_data = array();
+        foreach ($categories as $category) {
+            if ($category['top']) {
+                // Level 2
+                $children_data = array();
 
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+                $children = $this->model_catalog_category->getCategories($category['category_id']);
 
-				foreach ($children as $child) {
-					
-				$grandchildren_data = array();
+                foreach ($children as $child) {
+                    $grandchildren_data = array();
+                    // Level 3
+                    $grandchildren = $this->model_catalog_category->getCategories($child['category_id']);
+                    foreach ($grandchildren as $grandchild) {
 
-				// Level 3
-				$grandchildren = $this->model_catalog_category->getCategories($child['category_id']);
-				foreach ($grandchildren as $grandchild) {
-					
-					if ($this->config->get('config_product_count')) {
-					$total = ' (' . $this->model_catalog_product->getTotalProducts(array('filter_category_id'  => $grandchild['category_id'])). ')';
-					} else {
-					$total = '';
-					}
-					
-					$grandchildren_data[] = array(
-					'name' => $grandchild['name'] . $total,
-                        'category_id'     => $grandchild['category_id'],
-					'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '_' . $grandchild['category_id'])
-					);
-				 }
-				
-					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
-					
-					if ($this->config->get('config_product_count')) {
-					$total = ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')';
-					} else {
-					$total = '';
-					}
+                        if ($this->config->get('config_product_count')) {
+                            $total = ' (' . $this->model_catalog_product->getTotalProducts(array('filter_category_id' => $grandchild['category_id'])) . ')';
+                        } else {
+                            $total = '';
+                        }
 
+                        $grandchildren_data[] = array(
+                            'name'        => $grandchild['name'] . $total,
+                            'category_id' => $grandchild['category_id'],
+                            'href'        => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '_' . $grandchild['category_id'])
+                        );
+                    }
 
-					$children_data[] = array(
-						'name'  => $child['name'] . $total,
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
-                        'category_id'     => $child['category_id'],
-						'grandchildren'	=> $grandchildren_data,
-					);
-				}
+                    $filter_data = array(
+                        'filter_category_id'  => $child['category_id'],
+                        'filter_sub_category' => true
+                    );
 
+                    if ($this->config->get('config_product_count')) {
+                        $total = ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')';
+                    } else {
+                        $total = '';
+                    }
 
-                if($category['category_id'] == 308){
+                    $children_data[] = array(
+                        'name'          => $child['name'] . $total,
+                        'href'          => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+                        'category_id'   => $child['category_id'],
+                        'grandchildren' => $grandchildren_data,
+                    );
+                }
 
+                if ($category['category_id'] == 308) {
                     $children_data = array_reverse($children_data);
                 }
 
+                $total = $this->model_catalog_product->getTotalProducts($filter_data);
 
-				// Level 1
+                if ($category['name'] == 'OSOBNA NJEGA') {
+                    $total = 1;
+                }
+                // Level 1
+                if ($total != 0) {
+                    $data['categories'][] = array(
+                        'name'        => $category['name'],
+                        'children'    => $children_data,
+                        'category_id' => $category['category_id'],
+                        'column'      => $category['column'] ? $category['column'] : 1,
+                        'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
+                    );
+                }
+            }
+        }
 
-                if ($this->model_catalog_product->getTotalProducts($filter_data) != 0)
-				$data['categories'][] = array(
-					'name'     => $category['name'],
-					'children' => $children_data,
-                    'category_id'     => $category['category_id'],
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
-				);
-			}
-		}
-
-		return $this->load->view('common/menu', $data);
-	}
+        return $this->load->view('common/menu', $data);
+    }
 }
