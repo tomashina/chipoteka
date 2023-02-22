77f3ac03fbe8f9a46ecea59a4db3b6107df857b1
tomashina <tomislav@agmedia.hr>
2023-02-22 08:26:26 +0100

    fix images

--- upload/catalog/model/tool/image.php
+++ upload/catalog/model/tool/image.php
@@ -1,45 +1,45 @@
 <?php
 class ModelToolImage extends Model {
-	public function resize($filename, $width, $height) {
-		if (!is_file(DIR_IMAGE . $filename) || substr(str_replace('\\', '/', realpath(DIR_IMAGE . $filename)), 0, strlen(DIR_IMAGE)) != str_replace('\\', '/', DIR_IMAGE)) {
-			return;
-		}
+    public function resize($filename, $width, $height) {
+        if (!is_file(DIR_IMAGE . $filename) || substr(str_replace('\\', '/', realpath(DIR_IMAGE . $filename)), 0, strlen(DIR_IMAGE)) != str_replace('\\', '/', DIR_IMAGE)) {
+            return;
+        }
 
-		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-		$image_old = $filename;
-		$image_new = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.' . $extension;
+        $image_old = $filename;
+        $image_new = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.' . $extension;
         $image_new_webp = 'cachewebp/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.webp';
 
-		if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
-			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
-				 
-			if (!in_array($image_type, array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF, IMAGETYPE_WEBP))) {
-				return DIR_IMAGE . $image_old;
-			}
-						
-			$path = '';
+        if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
+            list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
 
-			$directories = explode('/', dirname($image_new));
+            if (!in_array($image_type, array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF, IMAGETYPE_WEBP))) {
+                return DIR_IMAGE . $image_old;
+            }
 
-			foreach ($directories as $directory) {
-				$path = $path . '/' . $directory;
+            $path = '';
 
-				if (!is_dir(DIR_IMAGE . $path)) {
-					@mkdir(DIR_IMAGE . $path, 0777);
-				}
-			}
+            $directories = explode('/', dirname($image_new));
 
-			if ($width_orig != $width || $height_orig != $height) {
-				$image = new Image(DIR_IMAGE . $image_old);
-				$image->resize($width, $height);
-				$image->save(DIR_IMAGE . $image_new);
-			} else {
-				copy(DIR_IMAGE . $image_old, DIR_IMAGE . $image_new);
-			}
-		}
+            /*	foreach ($directories as $directory) {
+                    $path = $path . '/' . $directory;
 
-       $gd = gd_info();
+                    if (!is_dir(DIR_IMAGE . $path)) {
+                        @mkdir(DIR_IMAGE . $path, 0777);
+                    }
+                }*/
+
+            if ($width_orig != $width || $height_orig != $height) {
+                //	$image = new Image(DIR_IMAGE . $image_old);
+//$image->resize($width, $height);
+                //	$image->save(DIR_IMAGE . $image_new);
+            } else {
+                //copy(DIR_IMAGE . $image_old, DIR_IMAGE . $image_new);
+            }
+        }
+
+        $gd = gd_info();
         if ($gd['WebP Support']) {
             if (!is_file(DIR_IMAGE . $image_new_webp) || (filectime(DIR_IMAGE . $image_new) > filectime(DIR_IMAGE . $image_new_webp))) {
                 $path = '';
@@ -59,15 +59,15 @@ class ModelToolImage extends Model {
                 $image_webp->save_webp(DIR_IMAGE . $image_new_webp);
             }
         }
-		
-		$image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
-		
-		if ($this->request->server['HTTPS']) {
-			//return $this->config->get('config_ssl') . 'image/' . $image_new;
+
+        $image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
+
+        if ($this->request->server['HTTPS']) {
+            //return $this->config->get('config_ssl') . 'image/' . $image_new;
             return 'https://images.chipoteka.hr/image/' . $image_new_webp . '" width="' . $width . '" height="' . $height;
-		} else {
-			//return $this->config->get('config_url') . 'image/' . $image_new;
+        } else {
+            //return $this->config->get('config_url') . 'image/' . $image_new;
             return 'https://images.chipoteka.hr/image/' . $image_new_webp . '" width="' . $width . '" height="' . $height;
-		}
-	}
+        }
+    }
 }
