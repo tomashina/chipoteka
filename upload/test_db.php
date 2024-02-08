c5914023833356edc20689452ab76282544e2803
Filip Jankoski <filip@agmedia.hr>
2024-02-08 15:23:24 +0100

    _fix: Cache exstensions & sizes.

--- upload/test_db.php
+++ upload/test_db.php
@@ -8,23 +8,23 @@ $range = cron_range('clean_product_images');
 $total = \Agmedia\Models\Product\Product::query()->count();
 
 $products = \Agmedia\Models\Product\Product::query()
-    ->where('luceed_uid', '!=', '')
-    ->where('image', '!=', '')
-    ->offset($range['offset'])
-    ->take($range['limit'])
-    ->get();
+                                           ->where('luceed_uid', '!=', '')
+                                           ->where('image', '!=', '')
+                                           ->offset($range['offset'])
+                                           ->take($range['limit'])
+                                           ->get();
 
 if ($range['offset'] > $total) {
     return json_encode(['success' => 'Import je gotov..!']);
 }
 
 foreach ($products as $product) {
-    $path = 'catalog/products/' . $product->luceed_uid;
+    $path         = 'catalog/products/' . $product->luceed_uid;
     $replace_path = $path . '/';
-    $images = [0 => str_replace($replace_path, '', $product->image)];
+    $images       = [0 => str_replace($replace_path, '', $product->image)];
 
     try {
-        $files = new DirectoryIterator(DIR_IMAGE . $path);
+        $files     = new DirectoryIterator(DIR_IMAGE . $path);
         $db_images = \Agmedia\Models\Product\ProductImage::query()->where('product_id', $product->product_id)->get();
 
         if ($db_images->count()) {
@@ -39,8 +39,23 @@ foreach ($products as $product) {
                     \Agmedia\Helpers\Log::store(DIR_IMAGE . $replace_path . $file->getFilename(), 'test');
 
                     //unlink(DIR_IMAGE . $replace_path . $file->getFilename());
-                    unlink(DIR_IMAGE . 'cache/' . $replace_path . $file->getFilename());
-                    unlink(DIR_IMAGE . 'cachewebp/' . $replace_path . $file->getFilename());
+                    //
+                    $name = str_replace('.jpg', '', $file->getFilename());
+                    $path = DIR_IMAGE . 'cache/' . $replace_path . $name;
+
+                    $c1_sizes = ['-50x50.jpg'];
+                    foreach ($c1_sizes as $size) {
+                        if (file_exists($path . $size)) {
+                            unlink($path . $size);
+                        }
+                    }
+
+                    $c2_sizes = ['-1155x1155.webp', '-550x550.webp', '-400x400.webp', '-130x130.webp', '-120x120.webp', '-80x80.webp'];
+                    foreach ($c2_sizes as $size) {
+                        if (file_exists($path . $size)) {
+                            unlink($path . $size);
+                        }
+                    }
                 }
             }
         }
@@ -51,6 +66,6 @@ foreach ($products as $product) {
 
 }
 
-//cron_range('clean_product_images', ($range['offset'] + $range['limit']), $range['limit']);
+cron_range('clean_product_images', ($range['offset'] + $range['limit']), $range['limit']);
 
 echo 'Done::';
