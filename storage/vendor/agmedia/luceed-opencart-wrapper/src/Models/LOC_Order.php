00592df26e521e8dfe02671859e4c748f81f24bd
tomashina <tomislav@agmedia.hr>
2024-03-29 12:24:10 +0100

    loc order remove number 101

--- storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_Order.php
+++ storage/vendor/agmedia/luceed-opencart-wrapper/src/Models/LOC_Order.php
@@ -188,7 +188,7 @@ class LOC_Order
         $this->items_available = false;//$this->setAvailability();
 
         $this->order = [
-            'nalog_prodaje_b2b'         => $this->oc_order['order_id']. '-' . Carbon::now()->year.'-101',
+            'nalog_prodaje_b2b'         => $this->oc_order['order_id']. '-' . Carbon::now()->year,
             'narudzba'                   => $this->oc_order['order_id'] . '-' . Carbon::now()->year,
             'datum'                     => Carbon::make($this->oc_order['date_added'])->format(agconf('luceed.date')),
             'skladiste'                 => '099',// 001 -> 099
