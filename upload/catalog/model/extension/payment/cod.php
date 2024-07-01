1f29a755fe432e9b6bb1bb4a77b289f282f917c8
tomashina <tomislav@agmedia.hr>
2024-07-01 11:28:38 +0200

    televizori

--- upload/catalog/model/extension/payment/cod.php
+++ upload/catalog/model/extension/payment/cod.php
@@ -12,6 +12,9 @@ class ModelExtensionPaymentCOD extends Model {
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
+
+        $data['cartData'] = $this->cart->getProducts();
+
 		if ($this->config->get('payment_cod_total') > 0 && $this->config->get('payment_cod_total') < $total) {
 			$status = false;
 		} elseif (!$this->cart->hasShipping()) {
@@ -20,13 +23,33 @@ class ModelExtensionPaymentCOD extends Model {
         elseif ($data['groupId'] > 2) {
             $status = false;
         }
+        elseif ($this->searchInArray($data['cartData'], "Televizor", "name") && $this->searchInArray($data['cartData'], "98incha", "name") || $this->searchInArray($data['cartData'], "85incha", "name")   || $this->searchInArray($data['cartData'], "77incha", "name")  || $this->searchInArray($data['cartData'], "75incha", "name")  || $this->searchInArray($data['cartData'], "65incha", "name")  || $this->searchInArray($data['cartData'], "55incha", "name")  || $this->searchInArray($data['cartData'], "58incha", "name")) {
+
+            if($this->searchInArray($data['cartData'], "NOKIA", "name") || $this->searchInArray($data['cartData'], "LUXOR", "name") || $this->searchInArray($data['cartData'], "STRONG", "name")){
+                $status = true;
+            } else{
+                $status = false;
+            }
+
+        }
 		elseif (!$this->config->get('payment_cod_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
-		} else {
+		}
+
+
+
+
+
+
+        else {
 			$status = false;
 		}
+
+
+
+
 
 		$method_data = array();
 
@@ -41,4 +64,14 @@ class ModelExtensionPaymentCOD extends Model {
 
 		return $method_data;
 	}
+
+    public function searchInArray($rawArray, $search, $column) {
+        foreach ($rawArray as $value) {
+            if (isset($value[$column]) && strpos($value[$column], $search) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
